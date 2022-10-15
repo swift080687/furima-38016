e176b8f7964128cbc028e34c40166a08e27c180c
swift080687 <swift080687@gmail.com>
2022-10-15 20:31:03 +0900

    商品情報編集機能_修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :seller_confirmation, only: [:edit]
+  before_action :seller_confirmation, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
